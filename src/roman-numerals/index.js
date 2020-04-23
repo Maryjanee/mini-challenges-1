a66dcc17c9bb629d6ad509c612699b4344372361
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-04-23 15:59:06 +0100

    Add map for base values of roman numerals

--- src/roman-numerals/index.js
+++ src/roman-numerals/index.js
@@ -4,6 +4,18 @@
  * @param {string} roman The all-caps Roman numeral between 1 and 3999 (inclusive).
  * @returns {number} The decimal equivalent.
  */
-function romanToDecimal(roman) {}
+function romanToDecimal(roman) {
+  const romanNumeral = roman.toUpperCase();
+  const romFigures = new Map([
+    [1, "I"],
+    [4, "IV"],
+    [5, "V"],
+    [9, "IX"][(10, "X")],
+    [40, "LX"][(50, "L")],
+    [90, "CX"][(100, "C")],
+    [400, "CD"][(500, "D")],
+    [900, "CM"][(1000, "M")],
+  ]);
+}
 
 module.exports = romanToDecimal;
