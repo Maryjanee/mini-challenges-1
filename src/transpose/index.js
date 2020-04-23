b4c7c717f02907044ac10b961398ea6e9c4e6aa9
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-04-23 18:24:50 +0100

    changed occurence of var to let

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -5,12 +5,12 @@
  * @returns {number[]} The transposed array
  */
 function transpose(array) {
-  var arr = [];
-  var temp_arr;
+  let arr = [];
+  let temp_arr;
 
-  for (var i = 0; i < array[0].length; i++) {
+  for (let i = 0; i < array[0].length; i++) {
     temp_arr = [];
-    for (var j = 0; j < array.length; j++) {
+    for (let j = 0; j < array.length; j++) {
       temp_arr.push(array[j][i]);
     }
     arr.push(temp_arr);
