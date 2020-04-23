189f323a77ccae37e8a16822aec8e7b136e6ba48
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-04-23 22:17:22 +0100

    add character count

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,10 +4,26 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {
-const strArr = Array.from(str);
-const acceptableChars = ["{","}","(", ")", "[", "]", ];
 
+function isValid(str) {
+  const acceptableChars = ["{", "}", "(", ")", "[", "]"];
+  let countObject = {};
+
+  const characterCount = (word, character) => {
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === character) {
+        count++;
+      }
+    }
+    return count;
+  };
+  for (let i = 0, l = acceptableChars.length; i < l; i++) {
+    let currentChar = acceptableChars[i];
+    countObject[currentChar] = characterCount(str, currentChar);
+  }
+
+  
 }
 
 module.exports = isValid;
