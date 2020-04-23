413432f9a02c5326a7b977b92dae67fbc289249f
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-04-23 18:00:23 +0100

    add js file for transpose

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,18 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+  var arr = [];
+  var temp_arr;
+
+  for (var i = 0; i < array[0].length; i++) {
+    temp_arr = [];
+    for (var j = 0; j < array.length; j++) {
+      temp_arr.push(array[j][i]);
+    }
+    arr.push(temp_arr);
+  }
+  return arr;
+}
 
 module.exports = transpose;
